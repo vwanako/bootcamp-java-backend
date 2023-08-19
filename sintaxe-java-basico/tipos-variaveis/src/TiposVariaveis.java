50915a6164c879c69c0c91d1316864aa6238c360
vic <118995708+vwanako@users.noreply.github.com>
2023-08-19 09:55:53 -0300

    declaração com diferentes tipos de variáveis

---
+++ sintaxe-java-basico/tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,12 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+
+        double salario = 1558.23;
+        float altura = 1.73F;
+        long views = 3_123_456L;
+        char letra = 'a';
+        boolean existe = true;
+        String name = "Athena";
+        byte idade = 18;
+    }
+}
