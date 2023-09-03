2128d5ed62ef4abc41200e0aeb71b70d893bca05
vic <118995708+vwanako@users.noreply.github.com>
2023-09-03 08:23:35 -0300

    desafios adicionados

--- projetos/ContaBanco/src/ContaTerminal.java
+++ projetos/ContaBanco/src/ContaTerminal.java
@@ -21,5 +21,6 @@ public class ContaTerminal {
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
                 + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
 
+        scanner.close();
     }
 }
---
+++ projetos/primeiros-desafios/EquilibrandoSaldo/src/EquilibrandoSaldo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EquilibrandoSaldo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldoAtual = scanner.nextDouble();
+        double valorDeposito = scanner.nextDouble();
+        double valorRetirada = scanner.nextDouble();
+
+        // TODO: Calcular o saldo atualizado de acordo com a descrição deste desafio.
+        saldoAtual = saldoAtual + valorDeposito - valorRetirada;
+
+        // TODO: Imprimir o a saída de conforme a tabela de exemplos (uma casa decimal).
+        String saldoFormatado = String.format("%.1f", saldoAtual);
+        System.out.println("Saldo atualizado na conta: " + saldoFormatado);
+
+        scanner.close();
+    }
+}
