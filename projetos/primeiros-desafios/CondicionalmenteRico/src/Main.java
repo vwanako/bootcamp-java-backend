54638e19957afa40e4eff3dd288b0c5941c9dedb
vic <118995708+vwanako@users.noreply.github.com>
2023-09-03 08:42:37 -0300

    desafio juros compostos adicionado

--- projetos/primeiros-desafios/CondicionalmenteRico/src/Main.java
+++ projetos/primeiros-desafios/CondicionalmenteRico/src/Main.java
@@ -17,5 +17,6 @@ public class Main {
       System.out.println("Saldo insuficiente. Saque nao realizado!");
     }
 
+    scanner.close();
   }
 }
---
+++ projetos/primeiros-desafios/JurosCompostos/src/BancoApp.java
@@ -0,0 +1,33 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class BancoApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+   
+        double valorInicial = scanner.nextDouble();
+        
+     
+        double taxaJuros = scanner.nextDouble();
+        
+        int periodo = scanner.nextInt();
+        
+        double valorFinal = valorInicial;
+
+        
+        //TODO: Iterar, baseado no período em anos, para calculo do valorFinal com os juros.
+        
+        double jurosCorrigido = 1 + taxaJuros;
+        double valorJuros = Math.pow(jurosCorrigido, periodo);
+        
+        valorFinal *= valorJuros;
+
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println("Valor final do investimento: R$ " + df.format(valorFinal));
+        
+        scanner.close();
+    }
+}
